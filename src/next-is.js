e0fb6e80cd8bfe538b52774d5b5000d302656c91
afeiship <1290657123@qq.com>
2017-08-12 09:45:15 +0800

    issue: add isNumber/... mehtods

--- src/next-is.js
+++ src/next-is.js
@@ -2,15 +2,6 @@
 
   var global = global || this || self || window;
   var nx = global.nx || require('next-js-core2');
-  var class2type = {};
-  var javascriptType = 'Boolean Number String Function Array Date RegExp Object Error';
-
-
-  //populate class2type map:
-  javascriptType.split(' ').forEach(function (inName) {
-    class2type['[object ' + inName + ']'] = inName.toLowerCase()
-  });
-
 
   nx.is = function (inTarget, inType) {
     if (inTarget && inTarget.is) {
@@ -45,8 +36,85 @@
   };
 
 
+  nx.isUndefined = function (inObj) {
+    return inObj === undefined;
+  };
+
+  nx.isNull = function (inObj) {
+    return inObj === null;
+  };
+
+  nx.isNil = function (inObj) {
+    return inObj == null;
+  };
+
+  nx.isNumber = function (inObj) {
+    return !isNaN(inObj) && typeof(inObj) == 'number';
+  };
+
+  nx.isBoolean = function (inObj) {
+    return typeof(inObj) == 'boolean';
+  };
+
+  nx.isString = function (inObj) {
+    return typeof(inObj) == 'string';
+  };
+
+  nx.isArray = Array.isArray || function (inObj) {
+      return inObj instanceof Array;
+    };
+
+  nx.isArrayLike = function (inObj) {
+    return typeof inObj.length == 'number';
+  };
+
+  nx.isFunction = function (inObj) {
+    return typeof(inObj) == 'function';
+  };
+
+  nx.isObject = function (inObj) {
+    return nx.type(inObj) == 'object';
+  };
+
+  nx.isDocument = function (inObj) {
+    return inObj != null && inObj.nodeType == 9;
+  };
+
+  nx.isWindow = function (inObj) {
+    return inObj != null && inObj == inObj.global;
+  };
+
+  nx.isPlainObject = function (inObj) {
+    return nx.isObject(inObj) && !nx.isWindow(inObj) && Object.getPrototypeOf(inObj) == Object.prototype;
+  };
+
+  nx.isEmptyObject = function (inObj) {
+    var key;
+    for (key in inObj) return false;
+    return true;
+  };
+
+  nx.isServer = function () {
+    return typeof module !== 'undefined' && module.exports
+  };
+
   if (typeof module !== 'undefined' && module.exports) {
     module.exports = nx.is;
+    exports.isUndefined = nx.isUndefined;
+    exports.isNull = nx.isNull;
+    exports.isNil = nx.isNil;
+    exports.isNumber = nx.isNumber;
+    exports.isBoolean = nx.isBoolean;
+    exports.isString = nx.isString;
+    exports.isArray = nx.isArray;
+    exports.isArrayLike = nx.isArrayLike;
+    exports.isFunction = nx.isFunction;
+    exports.isObject = nx.isObject;
+    exports.isDocument = nx.isDocument;
+    exports.isWindow = nx.isWindow;
+    exports.isPlainObject = nx.isPlainObject;
+    exports.isEmptyObject = nx.isEmptyObject;
+    exports.isServer = nx.isServer;
   }
 
 }());
