e4afc539c5f70f3cb837696f23021aa5bd6c346d
afeiship <afeiship@gmail.com>
2019-01-16 10:57:12 +0800

    add: new case

--- test/object-is.test.js
+++ test/object-is.test.js
@@ -1,11 +1,14 @@
 var nxIs = require('next-is');
+// https://www.cnblogs.com/snandy/p/4485229.html
 
 test('test object is: +0/-0 ___ 0', () => {
   var rs1 = nxIs(+0, 0);
   var rs2 = nxIs(-0, 0);
+  var rs3 = nxIs(-0, +0);
 
   expect(rs1).toBe(true);
   expect(rs2).toBe(false);
+  expect(rs3).toBe(false);
 });
 
 test('test object is:NaN', () => {
